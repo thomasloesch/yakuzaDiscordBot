e4cc64eaf50ae850320501077b53bbccc1e93b56
Thomas Loesch <tommy.loesch@gmail.com>
2019-12-13 01:10:32 -0500

    Added basic repo support to Work Command

--- src/main/java/org/github/thomasloesch/yakuzaBot/model/WorkCommandHandler.java
+++ src/main/java/org/github/thomasloesch/yakuzaBot/model/WorkCommandHandler.java
@@ -2,6 +2,7 @@ package org.github.thomasloesch.yakuzaBot.model;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class WorkCommandHandler extends DiscordCommandHandler {
@@ -9,15 +10,19 @@ public class WorkCommandHandler extends DiscordCommandHandler {
     private final String SUCCESS_RESULT_STRING = "You have been assigned as a: ";
 
     private ArrayList<String> possibleJobArr;
-
+    private IUserRepository userRepository;
+    private static String REPO_FILE_LOCATION = "C:\\Users\\Thomas Loesch\\IdeaProjects\\yakuzaBot\\repoFile.csv";
 
     public WorkCommandHandler() {
         InitializeJobArr();
+        userRepository = new FileUserRepository(new File(REPO_FILE_LOCATION));
     }
 
     private void InitializeJobArr() {
         possibleJobArr = new ArrayList<>();
         possibleJobArr.add("drug dealer");
+        possibleJobArr.add("hitman");
+        possibleJobArr.add("bouncer");
     }
 
     @Override
@@ -26,7 +31,15 @@ public class WorkCommandHandler extends DiscordCommandHandler {
         
         String jobString = message.substring(message.indexOf(' ')).toLowerCase().trim();
 
+        long userId = commandContext.getMessage().getUserAuthor().get().getId();
+
         if (possibleJobArr.contains(jobString)) {
+            if(userRepository.getUser(userId) == null) {
+                userRepository.saveUser(userId, jobString, 0);
+            } else {
+                userRepository.updateJob(userId, jobString);
+            }
+
             return new SuccessResult(jobString, SUCCESS_RESULT_STRING);
         }
 
